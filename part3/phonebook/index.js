729ad779152f796e7792df28c806294b1c12fc88
hfizhrsyid <hfizhrsyid21@gmail.com>
2025-07-02 16:50:54 +0700

    3.5

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -56,6 +56,29 @@ app.delete('/api/persons/:id', (request, response) => {
     response.status(204).end()
 })
 
+const generateRandomId = () => {
+  return Math.floor(Math.random() * 1000) + 1
+}
+
+app.post('/api/persons', (request, response) => {
+    const body = request.body
+
+    if (!body.name) {
+        return response.status(400).json({
+            error: "content missing"
+        })
+    }
+
+    const person = {
+        name: body.name,
+        number: body.number,
+        id: generateRandomId() 
+    }
+
+    persons = persons.concat(person)
+    response.json(persons)
+})
+
 const PORT = 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
