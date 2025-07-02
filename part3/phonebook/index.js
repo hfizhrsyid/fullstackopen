546ac3a74bb1e17a823574483fedd24b97c0518b
hfizhrsyid <hfizhrsyid21@gmail.com>
2025-07-02 16:12:36 +0700

    3.3

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -38,6 +38,16 @@ app.get('/info', (request, response) => {
     response.send(`<p>Phonebook has info for ${persons.length} people ${timeString}`)   
 })
 
+app.get('/api/persons/:id', (request, response) => {
+    const person = persons.find(p => p.id === request.params.id)
+
+    if (person) {
+        response.json(person)
+    } else {
+        response.status(204).end()
+    }
+})
+
 const PORT = 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
