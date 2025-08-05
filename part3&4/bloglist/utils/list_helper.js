e6368eea5b6d0a4215060323ed27025d74125396
hfizhrsyid <hfizhrsyid21@gmail.com>
2025-08-05 13:51:06 +0700

    4.4

--- part3&4/bloglist/utils/list_helper.js
+++ part3&4/bloglist/utils/list_helper.js
@@ -1,7 +1,12 @@
 const dummy = (blogs) => {
-  return 1
+    return 1
+}
+
+const totalLikes = (listOfBlogs) => {
+    const sumWithValue = listOfBlogs.reduce((accumulator, currentValue) => accumulator + currentValue.likes, 0)
+    return sumWithValue
 }
 
 module.exports = {
-  dummy
+    dummy, totalLikes
 }
